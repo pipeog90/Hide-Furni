9ef02dec8cb07cbbf42e9d6f39c251500ca93d4e
pipeog90 <pipeog90@hotmail.com>
2023-01-25 10:07:46 -0500

    Add files via upload

--- Hide_Furni_v4.0.py
+++ Hide_Furni_v4.0.py
@@ -2,6 +2,7 @@ import re
 import sys
 from g_python.gextension import Extension
 from g_python.hmessage import Direction
+from time import sleep
 
 extension_info = {
     "title": "Hide_Furni_v4.0",
@@ -19,10 +20,11 @@ mute_off = True  # allow the user to turn on or off msg with furni id when remov
 rollback_stock = []  # Stores furni information when removed, so it will be correctly rollback
 rollback_range = 0  # Gets number of furni that want to be rollback by user
 rollback_count = 0  # Gets the effective number of furni that were rollback
+not_rollback_count = 0  # Gets the number of furni that won't be rollback in the room
 
 
 def commands(message):
-    global hide_furni, rollback_range, rollback_count, mute_off
+    global hide_furni, rollback_range, rollback_count, mute_off, not_rollback_count
     text = message.packet.read_string().lower()
 
     if text == ":hide on" and hide_furni is True:
@@ -61,7 +63,10 @@ def commands(message):
                 break
         if mute_off:
             ext.send_to_client('{in:Chat}{i:123456789}{s:"[ Hide furni => Rollback ' + str(rollback_count)
-                               + ' furni ]"}{i:0}{i:30}{i:0}{i:0}')
+                               + ' furni ]"}{i:0}{i:1}{i:0}{i:0}')
+            if not_rollback_count > 0:
+                ext.send_to_client('{in:Chat}{i:123456789}{s:"[ Hide furni => ' + str(not_rollback_count)
+                                   + ' furni will not rollback ]"}{i:0}{i:1}{i:0}{i:0}')
         rollback_count = 0
 
 
@@ -105,40 +110,35 @@ def move_objects_floor(packet):
 
 
 def move_objects_wall(packet):
-    global furni_id, hide_furni, mute_off
+    global furni_id, hide_furni, mute_off, not_rollback_count
 
     [furni_id, x] = packet.packet.read("is")
 
     if hide_furni is True:
         ext.send_to_client('{in:ItemRemove}{s:"' + str(furni_id) + '"}{b:false}{i:0}{i:0}')
+        not_rollback_count += 1
         if mute_off:
             ext.send_to_client(
                 '{in:Chat}{i:123456789}{s:"[Furni Id ' + str(furni_id) + ' removed]"}{i:0}{i:30}{i:0}{i:0}')
 
 
 def get_move_objects_info(packet):
-    global furni_id, hide_furni, mute_off
+    global furni_id, hide_furni, mute_off, not_rollback_count
     if hide_furni is True:
         #print(packet.packet.read("iiiiissiisiii"))
         (furni_id, form, loc1, loc2, loc3, siz1, siz2, fix1, fix2, fix3, fix4, fix5, fix6) = packet.packet.read("iiiiissiisiii")
         if fix4 == -1:      # Do no stock in rollback furni that causes the extension to crash: LTD furni, staff only furni, etc.
             rollback_stock.insert(0, ["floor", furni_id, form, loc1, loc2, loc3, siz1, siz2, fix1, fix2, fix3, fix4, fix5])
         elif fix4 != -1 and mute_off:
-            ext.send_to_client('{in:Chat}{i:123456789}{s:"[ Hide furni => This furni cannot be rollback ]"}{i:0}{i:31}{i:0}{i:0}')
+            ext.send_to_client('{in:Chat}{i:123456789}{s:"[ Hide furni => This specific furni cannot be rollback ]"}{i:0}{i:31}{i:0}{i:0}')
+            not_rollback_count += 1
     # print(rollback_stock)
 
 
-def get_wall_objects_info(packet):
-    global furni_id, hide_furni
-    if hide_furni is True:
-        (furni_id, form, pos1, fix1, fix2, fix3, fix4) = packet.packet.read("sissiii")
-        rollback_stock.insert(0, ["wall", furni_id, form, pos1, fix1, fix2, fix3])
-        # print(rollback_stock)
-
-
 def reset_rollback(packet):
-    global rollback_stock
+    global rollback_stock, not_rollback_count
     rollback_stock = []  # reset range every time user enters a room
+    not_rollback_count = 0
 
 
 def block_error_message(message):
@@ -149,7 +149,6 @@ def block_error_message(message):
 ext.intercept(Direction.TO_SERVER, move_objects_floor, "MoveObject")
 ext.intercept(Direction.TO_SERVER, move_objects_wall, "MoveWallItem")
 ext.intercept(Direction.TO_CLIENT, get_move_objects_info, "ObjectUpdate")
-ext.intercept(Direction.TO_CLIENT, get_wall_objects_info, "ItemUpdate")
 ext.intercept(Direction.TO_SERVER, commands, "Chat")
 ext.intercept(Direction.TO_CLIENT, reset_rollback, "RoomEntryTile")
 ext.intercept(Direction.TO_CLIENT, block_error_message, "NotificationDialog")
